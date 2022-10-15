e25dcf8c04ca83eeec0c000991139525b589dc31
Igor Slobodskov <igor.slobodskov@phystech.edu>
2022-10-15 12:35:08 +0200

    refactor ServerConfig

--- src/main/scala/com/kright/fileswebserver/ServerConfig.scala
+++ src/main/scala/com/kright/fileswebserver/ServerConfig.scala
@@ -11,22 +11,29 @@ case class ServerConfig(port: Int,
                         maxThreadsCount: Int,
                         mappings: Seq[DirectoryMapping])
 
+object ServerConfig:
+  def apply(conf: Config): ServerConfig =
+    ServerConfig(
+      port = conf.getInt("port"),
+      maxThreadsCount = conf.getInt("maxThreadsCount"),
+      mappings = conf.get[java.util.ArrayList[Config]]("mapping").asScala.map(DirectoryMapping.apply).toSeq
+    )
+
+  def apply(): ServerConfig =
+    FileConfig.builder("conf").build()
+
+    Using(FileConfig.of("config.toml")) { conf =>
+      conf.load()
+      ServerConfig(conf)
+    }.get
+
+
 case class DirectoryMapping(fsPath: String,
                             browserPath: String)
 
-object ServerConfig:
-  def apply(): ServerConfig =
-    Using(FileConfig.of("config.toml")) { conf =>
-      conf.load()
-
-      ServerConfig(
-        port = conf.getInt("port"),
-        maxThreadsCount = conf.getInt("maxThreadsCount"),
-        mappings = conf.get[java.util.ArrayList[Config]]("mapping").asScala.map { c =>
-          DirectoryMapping(
-            fsPath = c.get[String]("fsPath"),
-            browserPath = c.get[String]("browserPath")
-          )
-        }.toSeq
-      )
-    }.get
+object DirectoryMapping:
+  def apply(conf: Config): DirectoryMapping =
+    DirectoryMapping(
+      fsPath = conf.get[String]("fsPath"),
+      browserPath = conf.get[String]("browserPath")
+    )
